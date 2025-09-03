fa5b830ea71beaafcbf2473e25195ade6d903b5f
MauricioLufin <mauricio.lufin@entersoft.cl>
2025-09-03 17:22:56 -0400

    Se creo archivos de componente de beneficiario

---
+++ src/app/features/ingreso-solicitud/beneficiario/beneficiario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beneficiario',
+  standalone: true,
+  imports: [],
+  templateUrl: './beneficiario.component.html',
+  styleUrl: './beneficiario.component.css'
+})
+export class BeneficiarioComponent {
+
+}
