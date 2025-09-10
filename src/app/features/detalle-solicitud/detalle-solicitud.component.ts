f0dfd5902fc2ff1f230e88fc4949d06d8f9ea996
opgomeza <omar.gomez@entersoft.cl>
2025-09-10 15:21:22 -0300

    reordenamiento dashboard, botones detalle solicitud

--- src/app/features/detalle-solicitud/detalle-solicitud.component.ts
+++ src/app/features/detalle-solicitud/detalle-solicitud.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MatFormField } from "@angular/material/form-field";
 import { InformacionGeneralComponent } from "./informacion-general/informacion-general.component";
 import { DocumentosAsociadosComponent } from "./documentosasociados/documentosasociados.component";
-import { CompaniasContactadasComponent } from "./companias-contactadas/companias-contactadas.component";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-detalle-solicitud',
   standalone: true,
-  imports: [MatFormField, InformacionGeneralComponent, DocumentosAsociadosComponent, CompaniasContactadasComponent],
+  imports: [MatFormField, InformacionGeneralComponent, DocumentosAsociadosComponent, MatButtonModule],
   templateUrl: './detalle-solicitud.component.html',
-  styleUrl: './detalle-solicitud.component.css'
+  styleUrl: './detalle-solicitud.component.css',
+  encapsulation:ViewEncapsulation.None
 })
 export default class DetalleSolicitudComponent {
 
---
+++ src/app/features/detalle-solicitud/devolver-con-observaciones/devolver-con-observaciones.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devolver-con-observaciones',
+  standalone: true,
+  imports: [],
+  templateUrl: './devolver-con-observaciones.component.html',
+  styleUrl: './devolver-con-observaciones.component.css'
+})
+export class DevolverConObservacionesComponent {
+
+}
