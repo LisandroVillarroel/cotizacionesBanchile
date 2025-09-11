13b31c5a1b89bdad270712b7c1b06091ad37a04a
opgomeza <omar.gomez@entersoft.cl>
2025-09-11 15:44:09 -0300

    se agregan bordes en dashboard, se crea el modal de devolver con observaciones

--- src/app/features/detalle-solicitud/detalle-solicitud.component.ts
+++ src/app/features/detalle-solicitud/detalle-solicitud.component.ts
@@ -1,17 +1,42 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { MatFormField } from "@angular/material/form-field";
 import { InformacionGeneralComponent } from "./informacion-general/informacion-general.component";
 import { DocumentosAsociadosComponent } from "./documentosasociados/documentosasociados.component";
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { DevolverConObservacionesComponent } from './devolver-con-observaciones/devolver-con-observaciones.component';
 
 @Component({
   selector: 'app-detalle-solicitud',
   standalone: true,
-  imports: [MatFormField, InformacionGeneralComponent, DocumentosAsociadosComponent, MatButtonModule],
+  imports: [MatFormField, InformacionGeneralComponent, DocumentosAsociadosComponent, MatButtonModule, DevolverConObservacionesComponent],
   templateUrl: './detalle-solicitud.component.html',
   styleUrl: './detalle-solicitud.component.css',
   encapsulation:ViewEncapsulation.None
 })
 export default class DetalleSolicitudComponent {
+  private readonly dialog = inject(MatDialog);
+  abrirDialogoYAvanzar(): void {
+    const dato = {
+      solicitudId: 'ID123456789',
+      fecha: '00 - 00 - 0000',
+      ejecutivo: 'Manuel Sepúlveda',
+    };
 
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    //Ajustes clave para evitar espacio en blanco
+    dialogConfig.width = '600px'; // Tamaño fijo y controlado
+    dialogConfig.maxHeight = '90vh'; // Altura máxima visible
+    dialogConfig.panelClass = 'custom-dialog-container'; // Clase para estilos personalizados
+    dialogConfig.data = dato;
+
+    this.dialog
+      .open(DevolverConObservacionesComponent, dialogConfig)
+      .afterClosed();
+  }
 }
+
--- src/app/features/detalle-solicitud/devolver-con-observaciones/devolver-con-observaciones.component.ts
+++ src/app/features/detalle-solicitud/devolver-con-observaciones/devolver-con-observaciones.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField } from "@angular/material/form-field";
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatDivider } from "@angular/material/divider";
 
+export interface DevolverConObservacionesData {
+  solicitudId: string;
+  fecha: string;
+  ejecutivo: string;
+  motivoDevolucion: string;
+
+}
 @Component({
   selector: 'app-devolver-con-observaciones',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatDialogModule, MatButtonModule, MatCardModule,
+    MatFormField, MatInputModule, FormsModule, MatDivider],
   templateUrl: './devolver-con-observaciones.component.html',
   styleUrl: './devolver-con-observaciones.component.css'
 })
-export class DevolverConObservacionesComponent {
 
+
+export class DevolverConObservacionesComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DevolverConObservacionesComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DevolverConObservacionesData
+  ) {}
+
+  cerrar(): void {
+    this.dialogRef.close();
+  }
+
+  confirmar(): void {
+    this.dialogRef.close('confirmado');
+  }
+
+  observaciones: string = '';
 }
