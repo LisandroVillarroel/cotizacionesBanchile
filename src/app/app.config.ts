0d0902318518963880ec26cd022acfbe9a29a31f
LisandroVillarroel <lisandrovillarroell@gmail.com>
2025-09-22 16:33:09 -0300

    se realiza el formateo dd/mm/yyy y se rescata fecha

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { Subscription } from 'rxjs';
+
+import { registerLocaleData } from '@angular/common';
+import id from '@angular/common/locales/id';
+
+registerLocaleData(id);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(withFetch(),),
-
+{ provide: LOCALE_ID, useValue: 'es-CL' },
   {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
 useValue: { appearance: 'outline', floatLabel: 'never', SubscripSizing:'dynamic' }
   }
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatLabel, MatHint } from "@angular/material/form-field";
 import { MatDatepickerModule} from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
@@ -10,6 +10,7 @@ import { ResumenGeneralComponent } from './resumen-general/resumen-general.compo
 import { SolicitudesGestionadasComponent } from './solicitudes-gestionadas/solicitudes-gestionadas.component';
 import DistribucionComponent from './distribucion/distribucion.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,19 +20,31 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatFormFieldModule,
     MatInputModule,
     MatLabel,
-    // MatHint,
     MatDatepickerModule,
     MatCardModule,
-    //DatePipe,
     ResumenGeneralComponent,
     SolicitudesGestionadasComponent,
     DistribucionComponent,
-    MatToolbarModule
+    MatToolbarModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormsModule,
+
   ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 
 export default class DashboardComponent {
-  fechaActual: Date = new Date();
+    fechaActual= new FormControl<Date>(new Date());
+
+
+    seleccionaFecha(){
+
+    //  console.log('fecha', this.fechaActual.value?.toISOString().split('T')[0]); // yyyy-mm-dd
+    //  console.log('fecha 2', this.fechaActual.value?.toLocaleDateString('es-CL')); // dd/mm/yyyy
+      const fechaFiltrar=this.fechaActual.value?.toLocaleDateString('es-CL');
+    }
+
 }
