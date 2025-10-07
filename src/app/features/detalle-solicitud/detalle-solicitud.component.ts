1cdc75424440916be84e77ef0504d6462fe97b9b
opgomeza <omar.gomez@entersoft.cl>
2025-10-07 17:21:46 -0300

    prueba

--- src/app/features/detalle-solicitud/detalle-solicitud.component.ts
+++ src/app/features/detalle-solicitud/detalle-solicitud.component.ts
@@ -48,8 +48,7 @@ import { EnviarACompaniaComponent } from './companias/enviar-a-compania/enviar-a
     MatTabsModule,
     CommonModule,
     BeneficiarioComponent,
-    AseguradoComponent,
-    DocumentosAsociadosComponent
+    AseguradoComponent
 ],
   templateUrl: './detalle-solicitud.component.html',
   styleUrl: './detalle-solicitud.component.css',
@@ -66,7 +65,7 @@ export default class DetalleSolicitudComponent {
   storage = inject(StorageService);
   _storage = signal(this.storage.get<ISesionInterface>('sesion'));
 
-  idSolicitudParametro=signal<string>('175')
+  idSolicitudParametro=signal<string>('')
   detalleService = inject(DetalleSolicitudService);
   infoGral = signal<ISolicitud | undefined>(undefined);
   //documentos = signal<IDocumento[] | undefined>(undefined);
@@ -115,100 +114,6 @@ export default class DetalleSolicitudComponent {
 
       },
     });
-/*     this.infoGral.set({
-      id_solicitud: 1,
-      rut_contratante: "88.888.901-9",
-      nombre_razon_social_contratante: "Carlos Torres Navarro",
-      id_rubro: 1,
-      nombre_rubro: "AUTOMOTRIZ",
-      id_tipo_seguro: 1,
-      nombre_tipo_seguro: "VEHICULO LIVIANO",
-      fecha_creacion_solicitud: "2025-09-22T15:08:24Z",
-      id_estado_solicitud: 1,
-      sla: "R"
-    });
-   /* this.asegurados.set([]);
-    this.beneficiarios.set([
-        {
-            rut_beneficiario: "1.615.222-2",
-            nombre_razon_social_beneficiario: "María López Fernández1615",
-            mail_beneficiario: "maria.lopez@gmail.com",
-            telefono_beneficiario: "+56 9 8882 2222",
-            region_beneficiario: "Valparaíso",
-            ciudad_beneficiario: "Valparaíso",
-            comuna_beneficiario: "Viña del Mar",
-            direccion_beneficiario: "Calle 5 Norte",
-            numero_dir_beneficiario: "456",
-            departamento_block_beneficiario: "",
-            casa_beneficiario: "1615"
-        },
-        {
-            rut_beneficiario: "8.882.222-2",
-            nombre_razon_social_beneficiario: "María López H1444",
-            mail_beneficiario: "maria.lopez1444@gmail.com",
-            telefono_beneficiario: "+56 9 8882 2222",
-            region_beneficiario: "Valparaíso",
-            ciudad_beneficiario: "Valparaíso",
-            comuna_beneficiario: "Viña del Mar",
-            direccion_beneficiario: "Calle 5 Norte",
-            numero_dir_beneficiario: "1444",
-            departamento_block_beneficiario: "",
-            casa_beneficiario: "14"
-        },
-        {
-            rut_beneficiario: "14.555.888-8",
-            nombre_razon_social_beneficiario: "Pepido P1234 NAso",
-            mail_beneficiario: "juan.1653@gmail.com",
-            telefono_beneficiario: "+56 9 1111 1111",
-            region_beneficiario: "Región Metropolitana",
-            ciudad_beneficiario: "Santiago",
-            comuna_beneficiario: "Providencia",
-            direccion_beneficiario: "Av. Providencia",
-            numero_dir_beneficiario: "1234",
-            departamento_block_beneficiario: "",
-            casa_beneficiario: ""
-        },
-        {
-            rut_beneficiario: "11.555.888-8",
-            nombre_razon_social_beneficiario: "Rodrigo acevedo",
-            mail_beneficiario: "juan.1653@gmail.com",
-            telefono_beneficiario: "+56 9 1111 1111",
-            region_beneficiario: "Región Metropolitana",
-            ciudad_beneficiario: "Santiago",
-            comuna_beneficiario: "Providencia",
-            direccion_beneficiario: "Av. Providencia",
-            numero_dir_beneficiario: "1234",
-            departamento_block_beneficiario: "",
-            casa_beneficiario: ""
-        },
-        {
-            rut_beneficiario: "11.340.888-8",
-            nombre_razon_social_beneficiario: "Gonzalo o acevedo",
-            mail_beneficiario: "juan.1653@gmail.com",
-            telefono_beneficiario: "+56 9 1111 1111",
-            region_beneficiario: "Región Metropolitana",
-            ciudad_beneficiario: "Santiago",
-            comuna_beneficiario: "Providencia",
-            direccion_beneficiario: "Av. Providencia",
-            numero_dir_beneficiario: "1134",
-            departamento_block_beneficiario: "",
-            casa_beneficiario: ""
-        },
-        {
-            rut_beneficiario: "8.232.165-3",
-            nombre_razon_social_beneficiario: "Pepido PagaTriple",
-            mail_beneficiario: "juan.1653@gmail.com",
-            telefono_beneficiario: "+56 9 8881 1653",
-            region_beneficiario: "Metropolitana de Santiago",
-            ciudad_beneficiario: "SANTIAGO",
-            comuna_beneficiario: "Providencia",
-            direccion_beneficiario: "Av. Providencia",
-            numero_dir_beneficiario: "1234",
-            departamento_block_beneficiario: "N",
-            casa_beneficiario: ""
-        }
-    ]);
-    this.observaciones.set([]); */
   }
 
   solicitudId: any;
@@ -236,75 +141,6 @@ export default class DetalleSolicitudComponent {
       .open(DevolverConObservacionesComponent, dialogConfig)
       .afterClosed();
   }
-
-
-
-
-
-
-
-
-
-
-//  devolverSolicitud(idSolicitud: number){
-//   console.log('devolverSolicitud idSolicitud',idSolicitud);
-//      this.detalleService.postDetalle(idSolicitud).subscribe({
-//       next: (dato: DetalleSolicitudInterface) => {
-//         if (dato.codigo === 200) {
-//           console.log('Detalle solicitud:', dato);
-//           this.infoGral.set({
-//             id_solicitud : this.idSolicitud,
-//             fecha_creacion_solicitud: dato.p_fecha_creacion_solicitud,
-//             rut_contratante: dato.p_rut_contratante,
-//             nombre_razon_social_contratante: dato.p_nombre_razon_social_contratante,
-//             id_rubro: dato.p_id_rubro,
-//             nombre_rubro: dato.p_nombre_rubro,
-//             id_tipo_seguro: dato.p_id_tipo_seguro,
-//             nombre_tipo_seguro: dato.p_nombre_tipo_seguro,
-//             sla: dato.p_sla,
-//             id_estado_solicitud: dato.p_id_estado_solicitud,
-//             nombre_estado: dato.p_nombre_estado
-//           });
-//           this.observaciones.set(dato.c_observaciones);
-//         } else {
-//           if (dato.codigo != 500) {
-//             console.log('Error:', dato.mensaje);
-//           } else {
-//             console.log('ERROR DE SISTEMA:');
-//           }
-//         }
-//       },
-//       error: (error) => {
-//         console.log('ERROR INESPERADO', error);
-//         console.log('ID Solicitud:', idSolicitud);
-
-//       },
-//     });
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   aprobarSolicitud(): void {
     const dato = {
