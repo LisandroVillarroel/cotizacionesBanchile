289cd0826e77af2f4fe94a44bef32a9410841028
opgomeza <omar.gomez@entersoft.cl>
2025-10-08 16:10:10 -0300

    xxx

--- src/app/features/ingreso-respuesta/informacion-principal/informacion-principal.component.ts
+++ src/app/features/ingreso-respuesta/informacion-principal/informacion-principal.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  input,
+  signal,
+  ViewChildren,
+  ElementRef,
+  QueryList,
+  inject,
+} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatDialog } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatDivider } from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
+import { StorageService } from '@shared/service/storage.service';
+import { ISesionInterface } from '@shared/modelo/sesion-interface';
 
 @Component({
   selector: 'app-informacion-principal',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatStepperModule,
+    MatCardModule,
+    MatDivider,
+  ],
   templateUrl: './informacion-principal.component.html',
   styleUrl: './informacion-principal.component.css'
 })
