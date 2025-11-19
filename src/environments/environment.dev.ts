d73692e9328f262945709ce0921684b5618382ca
lisandrovillarroel <lisandrovillarroell@gmail.com>
2025-11-19 11:22:53 -0300

    vuena a conexion entersoft

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  //apiUrlConsumer:'http://192.168.1.36:8083/ms-pseg-cotizaciones-consumer-solicitud/cotizaciones',
- apiUrlConsumer:'svc-ms-pseg-cotizaciones-consumer-solicitud.app-pseg-cotizaciones.svc.cluster.local',
+  apiUrlConsumer:'http://192.168.1.36:8083/ms-pseg-cotizaciones-consumer-solicitud/cotizaciones',
+ //apiUrlConsumer:'svc-ms-pseg-cotizaciones-consumer-solicitud.app-pseg-cotizaciones.svc.cluster.local',
   keyEcrypt: 'seretoBanChile',
 }
