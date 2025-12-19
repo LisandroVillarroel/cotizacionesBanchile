7f5008bbcabf87ebe6125fc208cbeda160f6de11
LisandroVillarroel <lisandrovillarroell@gmail.com>
2025-12-19 13:06:15 -0300

    version 25

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -33,7 +33,7 @@ import { Router } from '@angular/router';
         </a>
         <mat-divider [vertical]="true"></mat-divider>
         <span class="titulo mb-0" style="color: white;">
-          Plataforma Cotizaciones Seguros Empresa v.24
+          Plataforma Cotizaciones Seguros Empresa v.25
         </span>
       </div>
       <div class="usuario-container">
